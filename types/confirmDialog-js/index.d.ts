991e84716268ef55117541f1cafc16bc4c18d85a
allipierre <yottiallipierre40@yahoo.fr>
2017-11-20 15:05:27 +0100

    add Global Settings

--- types/confirmDialog-js/index.d.ts
+++ types/confirmDialog-js/index.d.ts
@@ -8,7 +8,7 @@ interface JQueryStatic {
    * confirm Dialog
    * @param {confirmOptions} pOtions
    */
-   confirm( pOtions: options.confirmOptions | string, title?:string): boolean | void | HTMLElement;
+   confirm( pOtions: options.confirmOptions | string, title?:string): boolean | void | HTMLElement | any;
 
   /**
    * confirm alert
@@ -29,7 +29,7 @@ interface JQuery {
    * confirm Dialog
    * @param {confirmOptions} pOtions
    */
-   confirm( pOtions: options.confirmOptions | string, title?:string): boolean | void | HTMLElement;
+   confirm( pOtions: options.confirmOptions | string, title?:string): boolean | void | HTMLElement | any;
 
   /**
    * confirm alert
@@ -44,19 +44,55 @@ interface JQuery {
    dialog( pOtions: options.confirmOptions | any):  void;
 }
 
+interface Window {
+    setContentAppend: any;
+}
+
+
+
 declare namespace options {
 
     interface confirmOptions {
             buttons? : buttonOptionss | any,
-            title? : string,
-            content?  : string,
-            onContentReady?:Function,
-            lazyOpen?:boolean
+            title? : string | boolean,
+            content?  : string | Function,
+            onContentReady?: Function,
+            lazyOpen?: boolean,
+            closeIcon?: boolean | Function,
+            type?: string,
+            typeAnimated?: boolean,
+            icon?: string,
+            closeIconClass?: string,
+            columnClass?: string,
+            containerFluid?: boolean,
+            boxWidth?: string,
+            useBootstrap?: boolean,
+            bootstrapClasses?: any,
+            draggable?: boolean,
+            dragWindowBorder?: boolean,
+            dragWindowGap?: number,
+            contentLoaded?: Function,
+            autoClose?: String,
+            backgroundDismiss?: boolean | Function | string,
+            backgroundDismissAnimation?: string,
+            escapeKey?: string | boolean,
+            onOpenBefore?:Function,
+            onOpen?: Function,
+            onClose?: Function,
+            onDestroy?: Function,
+            onAction?: Function
+
+
     }
 
     interface buttonOptionss {
-            cancel? : Function,
-            confirm? : Function
+            cancel?: Function,
+            confirm?: Function
     }
+
+}
+
+declare namespace jconfirm {
+   let defaults: any;
 
 }
